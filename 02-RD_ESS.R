d743abd1761972af03e8c6ed079f4e339eda56b5
Hongtao Zhang <49884198+squallteo@users.noreply.github.com>
2024-01-23 20:54:52 -0500

    update

---
+++ 02-RD_ESS.R
@@ -0,0 +1,51 @@
+library(tidyverse)
+library(ggplot2)
+library(mvtnorm)
+
+
+###################################
+RD_ess <- function(muvec, covmat, 
+                      rand_ratio=rand_ratio, correction=correction, grid_width=grid_width){
+  p1 <- seq(grid_width,1-grid_width, grid_width)
+  p0 <- seq(grid_width,1-grid_width, grid_width)
+  rddt <- expand_grid(p1, p0) %>% 
+    mutate(lp0=log(p0/(1-p0)), RD=p1-p0, 
+           var_iu=p1*(1-p1)/rand_ratio[1] + p0*(1-p0)/rand_ratio[2])
+  
+  J <- with(rddt, 1/(p0*(1-p0)))
+  tt <- with(rddt, cbind(lp0, RD))
+  bvnden <- dmvnorm(tt, mean=muvec, sigma=covmat)
+  
+  rddt <- rddt %>% mutate(J=J, bvnden=bvnden, transden=J*bvnden)
+
+  ess_iu <- (rddt$var_iu %*% rddt$transden) * grid_width^2 / s^2
+  ess_pt <- ess_iu * sum(rand_ratio)
+  
+  c(ess_iu, ess_pt)
+}
+
+
+theta0 <- 0.2
+s <- 0.1
+mu0 <- -1
+m0 <- 2
+corr <- -0.8
+muvec <- c(mu0, theta0)
+covmat <- matrix(c(m0^2, corr*m0*s, corr*m0*s, s^2), 2, 2)
+
+iu_size <- c(20, 10)
+iu_multiplier <- c(1, 5, 10, 20, 40, 50, 60)
+
+correction <- 0.5
+grid_width <- 0.01
+
+
+
+for(m in 1:length(iu_multiplier)){
+  rand_ratio <- iu_size * iu_multiplier[m]
+  rr <- RD_ess(muvec, covmat, rand_ratio=rand_ratio, correction=correction, grid_width=grid_width)
+  if(m==1){out <- rr}
+  else {out <- rbind(out, rr)}
+}
+out
+# write.csv(out, "out.csv")
---
+++ 02-RD_Newton.R
@@ -0,0 +1,66 @@
+grad_f <- function(par, n, y) {
+  a <- exp(par[1])
+  b <- 1 + a
+  g1 <- y[1] - n[1]*a/b + y[2]*((a/b + par[2])^-1)*(a/b^2) - 
+    (n[2]-y[2])*((1/b - par[2])^-1)*(a/b^2)
+  g2 <- y[2]*((a/b + par[2])^-1) - (n[2]-y[2])*((1/b - par[2])^-1)
+  c(g1, g2)
+}
+
+hessian_f <- function(par, n, y) {
+  a <- exp(par[1])
+  b <- 1 + a
+  H11 <- -n[1]*(a/b^2) + 
+    y[2]*(-((a/b + par[2])^-2)*(a/b^2)^2 + ((a/b + par[2])^-1)*(a*(1-a)/b^3)) -
+    (n[2]-y[2])*(((1/b - par[2])^-2)*(a/b^2)^2 + ((1/b - par[2])^-1)*(a*(1-a)/b^3)) 
+  H12 <- -y[2]*((a/b + par[2])^-2)*(a/b^2) - (n[2]-y[2])*((1/b - par[2])^-2)*(a/b^2)
+  H22 <- -y[2]*((a/b + par[2])^-2) - (n[2]-y[2])*((1/b - par[2])^-2)
+  matrix(c(H11, H12, H12, H22), nrow = 2)
+}
+
+
+#Newton Raphson
+newton_raphson <- function(initial_guess, n, y, tolerance = 1e-5, max_iter = 100) {
+  x <- initial_guess
+  iter <- 0
+  
+  while (iter < max_iter) {
+    # Calculate gradient and Hessian at the current point
+    gradient <- grad_f(par=x, n=n, y=y)
+    hessian <- hessian_f(par=x, n=n, y=y)
+    
+    # Check for singularity of the Hessian
+    if (det(hessian) == 0) {
+      stop("Hessian is singular. Unable to continue.")
+    }
+    
+    # Update the parameter using Newton-Raphson update
+    x <- x - solve(hessian) %*% gradient
+    
+    # Check for convergence
+    if (sqrt(sum(gradient^2)) < tolerance) {
+      break
+    }
+    
+    iter <- iter + 1
+  }
+  
+  # Calculate the covariance matrix using the inverse of the Hessian
+  covariance_matrix <- solve(-hessian)
+  
+  return(list(x = x, covariance_matrix = covariance_matrix, iterations = iter))
+}
+
+
+#par: l0 and theta
+#n, y: control and treatment
+n <- c(100, 200)
+y <- c(22, 70)
+p <- y/n
+initial_guess <- c(0,0)
+# grad_f(initial_guess, n, y)
+# hessian_f(initial_guess, n, y)
+
+result <- newton_raphson(initial_guess, n=n, y=y)
+result$covariance_matrix[1,2] / sqrt(result$covariance_matrix[1,1]*result$covariance_matrix[2,2])
+
