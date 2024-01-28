81b5e98c742925fb59279b9302cca9bb27a5ea65
Hongtao Zhang <49884198+squallteo@users.noreply.github.com>
2024-01-28 13:12:26 -0500

    update

--- 02-RD_Newton.R
+++ 02-RD_Newton.R
@@ -1,26 +1,24 @@
-grad_f <- function(par, n, y) {
-  a <- exp(par[1])
-  b <- 1 + a
-  g1 <- y[1] - n[1]*a/b + y[2]*((a/b + par[2])^-1)*(a/b^2) - 
-    (n[2]-y[2])*((1/b - par[2])^-1)*(a/b^2)
-  g2 <- y[2]*((a/b + par[2])^-1) - (n[2]-y[2])*((1/b - par[2])^-1)
-  c(g1, g2)
-}
-
-hessian_f <- function(par, n, y) {
-  a <- exp(par[1])
-  b <- 1 + a
-  H11 <- -n[1]*(a/b^2) + 
-    y[2]*(-((a/b + par[2])^-2)*(a/b^2)^2 + ((a/b + par[2])^-1)*(a*(1-a)/b^3)) -
-    (n[2]-y[2])*(((1/b - par[2])^-2)*(a/b^2)^2 + ((1/b - par[2])^-1)*(a*(1-a)/b^3)) 
-  H12 <- -y[2]*((a/b + par[2])^-2)*(a/b^2) - (n[2]-y[2])*((1/b - par[2])^-2)*(a/b^2)
-  H22 <- -y[2]*((a/b + par[2])^-2) - (n[2]-y[2])*((1/b - par[2])^-2)
-  matrix(c(H11, H12, H12, H22), nrow = 2)
-}
-
-
-#Newton Raphson
-newton_raphson <- function(initial_guess, n, y, tolerance = 1e-5, max_iter = 100) {
+rd_est <- function(initial_guess, n, y, tolerance = 1e-5, max_iter = 100) {
+  grad_f <- function(par, n, y) {
+    a <- exp(par[1])
+    b <- 1 + a
+    g1 <- y[1] - n[1]*a/b + y[2]*((a/b + par[2])^-1)*(a/b^2) - 
+      (n[2]-y[2])*((1/b - par[2])^-1)*(a/b^2)
+    g2 <- y[2]*((a/b + par[2])^-1) - (n[2]-y[2])*((1/b - par[2])^-1)
+    c(g1, g2)
+  }
+  
+  hessian_f <- function(par, n, y) {
+    a <- exp(par[1])
+    b <- 1 + a
+    H11 <- -n[1]*(a/b^2) + 
+      y[2]*(-((a/b + par[2])^-2)*(a/b^2)^2 + ((a/b + par[2])^-1)*(a*(1-a)/b^3)) -
+      (n[2]-y[2])*(((1/b - par[2])^-2)*(a/b^2)^2 + ((1/b - par[2])^-1)*(a*(1-a)/b^3)) 
+    H12 <- -y[2]*((a/b + par[2])^-2)*(a/b^2) - (n[2]-y[2])*((1/b - par[2])^-2)*(a/b^2)
+    H22 <- -y[2]*((a/b + par[2])^-2) - (n[2]-y[2])*((1/b - par[2])^-2)
+    matrix(c(H11, H12, H12, H22), nrow = 2)
+  }
+  
   x <- initial_guess
   iter <- 0
   
@@ -54,13 +52,10 @@ newton_raphson <- function(initial_guess, n, y, tolerance = 1e-5, max_iter = 100
 
 #par: l0 and theta
 #n, y: control and treatment
-n <- c(100, 200)
-y <- c(22, 70)
-p <- y/n
-initial_guess <- c(0,0)
-# grad_f(initial_guess, n, y)
-# hessian_f(initial_guess, n, y)
-
-result <- newton_raphson(initial_guess, n=n, y=y)
-result$covariance_matrix[1,2] / sqrt(result$covariance_matrix[1,1]*result$covariance_matrix[2,2])
-
+# n <- c(100, 200)
+# y <- c(22, 70)
+# initial_guess <- c(0,0)
+# 
+# result <- rd_est(initial_guess, n=n, y=y)
+# result$covariance_matrix[1,2] / sqrt(result$covariance_matrix[1,1]*result$covariance_matrix[2,2])
+# 
