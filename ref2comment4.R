26efd7c9beaa5c15209b5e10a7882fcaa4b5d038
squallteo <squallteo@gmail.com>
2024-12-18 21:42:40 -0500

    update

--- ref2comment4.R
+++ ref2comment4.R
@@ -1,11 +1,12 @@
 library(RBesT)
 
+#107/245
 mu0 <- -0.254
 m0 <- sqrt(0.0166)
 
-tt <- rnorm(1000, mu0, m0)
+tt <- rnorm(10000, mu0, m0)
 tt1 <- inv_logit(tt)
 
-fit <- automixfit(tt1, type = "beta", Nc=1:6)
+fit <- automixfit(tt1, type = "beta", Nc=1:4)
 
 ess(fit)
